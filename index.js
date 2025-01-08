b6a2af151ee9b7a2a75819ade6ee3b2dd42d102c
muhammadnehal610 <153855864+muhammadnehal610@users.noreply.github.com>
2025-01-08 10:54:23 +0500

    auth

--- index.js
+++ index.js
@@ -1,50 +1,25 @@
-import express from "express";
-import "dotenv/config";
-import mongoose from "mongoose";
-import cors from "cors";
-import morgan from "morgan";
+import express from 'express'
 
-import UsersRoutes from "./routers/users.js";
-import authRoutes from "./routers/auth.js";
-import taskRoutes from "./routers/task.js";
-import studentsRoutes from "./routers/students.js";
-import uploadRoutes from "./routers/upload.js";
+
+
+import connectDB from './config/database.js';
+
+import userRoute from "./routes/authRoutes.js";
+
 
 const app = express();
-const PORT = 4040;
+
+// Connect to MongoDB
+connectDB();
 
 // Middleware
 app.use(express.json());
-app.use(morgan("dev"));
-
-const corsOptions = {
-  origin: "http://localhost:5173", // Frontend URL
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
-};
-app.use(cors(corsOptions));
-
-// MongoDB Connection
-mongoose
-  .connect(process.env.MONGOBDURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB is connected"))
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-    process.exit(1);
-  });
 
 // Routes
-app.use("/users", UsersRoutes);
-app.use("/auth", authRoutes);
-app.use("/task", taskRoutes);
-app.use("/students", studentsRoutes);
-app.use("/upload", uploadRoutes);
-
-app.get("/", (req, res) => {
-  res.send("My first app is running");
-});
+app.use("/api/auth", userRoute);
 
 // Start Server
+const PORT = 4040;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
--- models/user.js
+++ models/user.js
@@ -1,23 +1,10 @@
 import mongoose from "mongoose";
-const { Schema } = mongoose;
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, unique: true },
-    password: { type: String},
-    nic: { type: String},
-    advance: { type: Number },
-    role: { type: String, enum: ['user', 'admin'], default: 'user' },
-    room: { type: Number},
-    startDate: { type: Date},
-    city: { type: String},
-    country: { type: String },
-    portionNumber: { type: Number },
-    monthlyRent: { type: Number},
-    nicPicture: { type: String },
-    contactNumber: { type: String},
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, default: "user" },
 }, { timestamps: true });
 
-const User = mongoose.model("User", userSchema);
-export default User;
-
+export default mongoose.model("User", userSchema);

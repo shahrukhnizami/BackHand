a43baed48b039ae44147ed57fc55f2da568ce3b7
shahrukhnizami <nizamishahrukh12@gmail.com>
2025-01-25 03:11:01 +0500

    uk

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use(cors());
 app.use(
   cors({
     // origin: "http://localhost:5173", // Replace with your frontend's URL
-    origin: "frontend-rms.vercel.app", // Replace with your frontend's URL
+    // origin: "frontend-rms.vercel.app", // Replace with your frontend's URL
+    origin: "https://frontend-fbx38u5yl-shahrukhs-projects-ff5f4f54.vercel.app", // Replace with your frontend's URL
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true, // If you're using cookies or authentication headers
   })
